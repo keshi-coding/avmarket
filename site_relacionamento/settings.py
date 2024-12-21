17de2a2d531657be6069908ff5e0728417c556aa
Keshi <keshi.coding@gmail.com>
2024-12-22 01:28:51 +0900

    configuracoes e progresso no projeto site-relacionamento

--- site_relacionamento/settings.py
+++ site_relacionamento/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import ssl
+import smtplib
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -27,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meu_app',
+
+    # django-allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+# django-allauth requer a identificação de qual site
+SITE_ID = 1
+
+# Redirecionamento após login
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'site_relacionamento.urls'
@@ -69,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'site_relacionamento.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -83,7 +96,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -103,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -115,7 +126,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -125,3 +135,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuração de e-mails
+EMAIL_BACKEND = 'site_relacionamento.settings.CustomSMTPEmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'keshi.coding@gmail.com'
+EMAIL_HOST_PASSWORD = 'jdla vjyr ibsz azzp'
+DEFAULT_FROM_EMAIL = 'keshi.coding@gmail.com'
+
+# Autenticação do Django Allauth
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # Verificação de e-mail
+ACCOUNT_USERNAME_REQUIRED = True  # Login por nome de usuário
+ACCOUNT_AUTHENTICATION_METHOD = "username"  # Autenticação por nome de usuário
+ACCOUNT_EMAIL_REQUIRED = True  # E-mail obrigatório
+ACCOUNT_SIGNUP_REDIRECT_URL = "/"  # Redirecionamento após cadastro
